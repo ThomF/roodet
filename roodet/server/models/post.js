3eabe715983fa9a76dd6f8803a76a49da66bc660
ThomF <seniorswanky@icloud.com>
2023-05-15 03:18:52 -0600

    c

---
+++ roodet/server/models/post.js
@@ -0,0 +1,13 @@
+import { Schema } from "mongoose";
+import { basicStringType } from "../db/Constants.js";
+
+export const PostSchema = new Schema({
+
+    creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
+    title: { type: String, maxLength: 100, required: true },
+    body: { type: String, required: true },
+    isLocked: { type: Boolean, default: false, required: true },
+    subRood: { type: String, required: true }
+    // TODO Look into flares
+
+}, { timestamps: true, toJSON: { virtuals: true } })
